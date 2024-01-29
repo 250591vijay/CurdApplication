47e383634528899e1fff0ae1f7f0b818f8e30a17
VijayKumar <250591vijay@gmail.com>
2024-01-29 16:56:42 +0530

    Laptop controller for testing purpose

---
+++ Controllers/ImageImplementation/LaptopController.cs
@@ -0,0 +1,20 @@
+﻿using CurdApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurdApplication.Controllers.ImageImplementation
+{
+    public class LaptopController : Controller
+    {
+        private ApplicationContext _context;
+
+        public LaptopController(ApplicationContext context)
+        {
+            this._context=context;
+        }
+        public IActionResult Index()
+        {
+         var data = _context.Laptops.ToList();   
+            return View();
+        }
+    }
+}
